a44010a449f4b233fb6c60948468e7c333f8652d
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2019-02-18 22:16:47 +0100

    surface offset & mapping update

--- UR_Control/geometry/surface_offset.py
+++ UR_Control/geometry/surface_offset.py
@@ -3,10 +3,11 @@
 import Rhino.Geometry as rg
 import copy
 
-def offset_sides_surface(srf, value = 20):
+### change srf to self, ^^^ NEED THE python.copy LIBRARY!!! ^^^
+def offset_sides_surface(srf, offset_dis = 20):
     """ method that returns a slightly shrunk version of the surface along the v direction
 
-        :param value:       Offset Distance (default 20)
+        :param offset_dis:       Offset Distance (default 20)
         :return temp_srf:   The trimmed surface
     """
     sampling_count = 25
@@ -14,27 +15,32 @@ def offset_sides_surface(srf, value = 20):
     temp_surface = copy.deepcopy(srf)
 
     temp_surface.SetDomain(1, rg.Interval(0, sampling_count - 1))
-    print temp_surface.Domain(1)
     temp_isocurves = [temp_surface.IsoCurve(0, v_val) for v_val in range(sampling_count)]
 
     temp_isocurves_shortened = []
+
     for isocurve in temp_isocurves:
+        # getting the length and the domain of every isocurve
         length = isocurve.GetLength()
         start_t, end_t = isocurve.Domain[0], isocurve.Domain[1]
         t_delta = end_t - start_t
         t_differential = t_delta / length
-        t_shift = t_differential * value
+        # calculating how much the offset_dis result in t_val change
+        t_shift = t_differential * offset_dis
         start_t_new = start_t + t_shift
         end_t_new = end_t - t_shift
+        # splitting the curve at those two new start & end points with and taking the middle piece
         new_isocurve = isocurve.Split([start_t_new, end_t_new])[1]
         temp_isocurves_shortened.append(new_isocurve)
 
+    # lofting those isocurves
     loft_type = rg.LoftType.Tight
     new_surface = rg.Brep.CreateFromLoftRebuild(temp_isocurves_shortened, rg.Point3d.Unset, rg.Point3d.Unset, loft_type, False, 50)[0]
-    print new_surface.ToNurbsSurface()
+    # getting the loft as a nurbssurface out of the resulting brep
+    print new_surface
+    print new_surface.Faces.Item[0].ToNurbsSurface()
 
-
-    return temp_isocurves_shortened, new_surface
+    return new_surface
 
 # grasshopper variables
 
@@ -42,4 +48,4 @@ srf
 value
 
 # return values
-isocurves, temp_surface = offset_sides_surface(srf, value)
+temp_surface = offset_sides_surface(srf, value)
