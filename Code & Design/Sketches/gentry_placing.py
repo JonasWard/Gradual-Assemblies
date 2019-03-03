1b2789150c273bcdc810a7b1ec9389b7c5ae7bb5
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2019-03-03 15:19:43 +0100

    gentry_location_update

--- Code & Design/Sketches/gentry_placing.py
+++ Code & Design/Sketches/gentry_placing.py
@@ -1,15 +1,23 @@
 # pseudocode that might define location of the gentry
 # for given placement point and static z_value, radius and angle
 
-x1, y1, z1 = placement_plane.Origin
-x0 = value
-r = 1700    # in mm
-alfa = 1.05
+import Rhino.Geometry as rg
 
-delta_z = .86 * r       # cos 30 degree - could be slightly more or less if need be
+x1, y1, z1 = placement_pt.X, placement_pt.Y, placement_pt.Z
+x0 = x_plane.X
+r = 1700    # in mm
+
+sphere = rg.Sphere(placement_pt, r).ToBrep()
+print sphere
+x_plane = rg.Plane.WorldYZ
+x_plane.Translate(rg.Vector3d(x0, 0, 0))
+ignore, curves, ignore = rg.Intersect.Intersection.BrepPlane(sphere, x_plane, .01)
+a = curves
+
+delta_z = .7 * r       # cos 30 degree - could be slightly more or less if need be
 z0 = z1 - delta_z
 delta_x = x1 - x0
-y0 = (r ** 2 - delta_z ** 2 - delta_x ** 2) ** .5    # has to change depending on the location
+delta_y = (r ** 2 - delta_z ** 2 - delta_x ** 2) ** .5    # has to change depending on the location
 
 extra_value = 1000      # periscoper spacing?
-gentry_location = pt(x0, y0, z0 + extra_value)
+gentry_location = rg.Point3d(x0, y1 - delta_y, z1  - delta_z)
