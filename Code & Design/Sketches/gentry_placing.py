7aba84fd18c0d70f2a6bcadd6db144f26bbaf50c
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2019-03-03 16:41:39 +0100

    vavevd

--- Code & Design/Sketches/gentry_placing.py
+++ Code & Design/Sketches/gentry_placing.py
@@ -1,23 +1,36 @@
-# pseudocode that might define location of the gentry
-# for given placement point and static z_value, radius and angle
+def placement_sequence_gentry_position(pick_pl, place_pl):
 
-import Rhino.Geometry as rg
+	# setting variables
+	pick_b_pt = pick_pl.Origin
+	place_b_pt = place_pl.Origin
 
-x1, y1, z1 = placement_pt.X, placement_pt.Y, placement_pt.Z
-x0 = x_plane.X
-r = 1700    # in mm
+	cos_angle = .7
+	sphere_radius = 1700
 
-sphere = rg.Sphere(placement_pt, r).ToBrep()
-print sphere
-x_plane = rg.Plane.WorldYZ
-x_plane.Translate(rg.Vector3d(x0, 0, 0))
-ignore, curves, ignore = rg.Intersect.Intersection.BrepPlane(sphere, x_plane, .01)
-a = curves
 
-delta_z = .7 * r       # cos 30 degree - could be slightly more or less if need be
-z0 = z1 - delta_z
-delta_x = x1 - x0
-delta_y = (r ** 2 - delta_z ** 2 - delta_x ** 2) ** .5    # has to change depending on the location
+	# picking position
+	x1, y1, z1 = pick_b_pt.X, pick_b_pt.Y, pick_b_pt.Z
+	x0 = x_plane.X 		# replace with gentry vals
+	r = 1700    # in mm
 
-extra_value = 1000      # periscoper spacing?
-gentry_location = rg.Point3d(x0, y1 - delta_y, z1  - delta_z)
+	delta_z = cos_angle * sphere_radius       # cos 30 degree - could be slightly more or less if need be
+	z0 = z1 - delta_z
+	delta_x = x1 - x0
+	delta_y = (sphere_radius ** 2 - delta_z ** 2 - delta_x ** 2) ** .5    # has to change depending on the location
+
+	gentry_pick_pl = rg.Plane.WorldXY
+	gentry_pick_pl.Translate(rg.Vector3d(x0, y1 - delta_y, z0))
+
+	# placement position
+	x1, y1, z1 = place_b_pt.X, place_b_pt.Y, place_b_pt.Z
+	x0 = x_plane.X
+
+	delta_z = cos_angle * sphere_radius       # cos 30 degree - could be slightly more or less if need be
+	z0 = z1 - delta_z		# replace with gentry vals
+	delta_x = x1 - x0
+	delta_y = (sphere_radius ** 2 - delta_z ** 2 - delta_x ** 2) ** .5    # has to change depending on the location
+
+	gentry_place_pl = rg.Plane.WorldXY
+	gentry_place_pl.Translate(rg.Vector3d(x0, y1 + delta_y, z0))
+
+	return gentry_pick_pl, gentry_place_pl
